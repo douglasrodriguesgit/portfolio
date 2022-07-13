7fb79ceb47941a63aeca1f4e3765b82cd86b7678
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2022-07-12 22:11:49 -0300

    adicionado css

---
+++ script/script.js
@@ -0,0 +1,36 @@
+function scrollSuave() {
+  const navMenu = document.querySelectorAll('.js-menu a[href^="#"]');
+  function scrollMenu(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const menu = document.querySelector(href);
+    menu.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  navMenu.forEach((link) => {
+    link.addEventListener("click", scrollMenu);
+  });
+}
+scrollSuave();
+
+
+
+function initAnimacaoScroll() {
+  const sectionScroll = document.querySelectorAll(".js-scroll");
+  if (sectionScroll.length) {
+    const windowMetade = innerHeight * 0.6;
+    function animaScroll() {
+      sectionScroll.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const sectionInvisible = sectionTop - windowMetade < 0;
+        if (sectionInvisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimacaoScroll(); 
